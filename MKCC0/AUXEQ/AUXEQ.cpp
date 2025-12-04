9913050a6176127d6530c980eddc71a135d6cb76
SMH-MRD <masato.kobayashi@shi-g.com>
2025-12-04 18:07:25 +0900

    update

--- MKCC0/AUXEQ/AUXEQ.cpp
+++ MKCC0/AUXEQ/AUXEQ.cpp
@@ -173,7 +173,7 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
    DWORD	str_num = GetPrivateProfileString(SYSTEM_SECT_OF_INIFILE, ODER_CODE_KEY_OF_INIFILE, L"XXXXXX00", pEnvStat->device_code.crane_id, _countof(pEnvStat->device_code.crane_id), PATH_OF_INIFILE);
    str_num = GetPrivateProfileString(SYSTEM_SECT_OF_INIFILE, PC_TYPE_KEY_OF_INIFILE, L"XXXX", pEnvStat->device_code.pc_type, _countof(pEnvStat->device_code.pc_type), PATH_OF_INIFILE);
 
-   WCHAR wbuf[16];
+   WCHAR wbuf[32];
    str_num = GetPrivateProfileString(SYSTEM_SECT_OF_INIFILE, PC_SERIAL_KEY_OF_INIFILE, L"0", wbuf, 32, PATH_OF_INIFILE);
    swscanf_s(wbuf, L"%d", &(pEnvStat->device_code.serial_no));
 
